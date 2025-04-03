67e8a57f5328384751e395823263ed36042f1c6e
elissegomez24 <elissegomez24@gmail.com>
2025-04-03 19:46:07 -0400

    Updated the Vote Model

--- seeds.py
+++ seeds.py
@@ -2,6 +2,7 @@ from app.models import User
 from app.db import Session, Base, engine
 from app.models import User, Post
 from app.models import User, Post, Comment
+from app.models import User, Post, Comment, Vote 
 
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
@@ -38,6 +39,17 @@ db.add_all([
   Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
   Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
   Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+])
+
+db.commit()
+
+# insert votes
+db.add_all([
+  Vote(user_id=1, post_id=2),
+  Vote(user_id=1, post_id=4),
+  Vote(user_id=2, post_id=4),
+  Vote(user_id=3, post_id=4),
+  Vote(user_id=4, post_id=2)
 ])
 
 db.commit()
